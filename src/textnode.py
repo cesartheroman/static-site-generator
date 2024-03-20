743fdc8d570ca62c2a06f462e6d120791498e2c7
Cesar Roman <cesardroman18@gmail.com>
2024-03-20 17:03:32 -0500

    refactor: changed function names for class

--- src/textnode.py
+++ src/textnode.py
@@ -4,12 +4,12 @@ class TextNode:
         self.text_type = text_type
         self.url = url
 
-    def eq(self, other_node):
+    def __eq__(self, other_node):
         return (
-            (self.text == other_node.text)
-            and (self.text == other_node.text_type)
-            and (self.url == other_node.url)
+            self.text == other_node.text
+            and self.text == other_node.text_type
+            and self.url == other_node.url
         )
 
-    def repr(self):
-        print(f"TextNode({self.text}, {self.text_type}, {self.url})")
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
