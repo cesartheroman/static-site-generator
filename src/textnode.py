53df98694788b1ea8d91e12bf803b8db554f877a
Cesar Roman <cesardroman18@gmail.com>
2024-04-29 20:34:13 -0500

    fix: added self to instance

--- src/textnode.py
+++ src/textnode.py
@@ -38,7 +38,7 @@ class TextNode:
         """
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
-    def text_node_to_html_node(text_node):
+    def text_node_to_html_node(self, text_node):
         """
         Should handle each type of TextNode:
         text_type_text = 'text'
