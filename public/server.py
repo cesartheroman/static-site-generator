641e25ca8267ea4587417f4594a00c009698cc92
cesartheroman <cesardroman18@gmail.com>
2024-10-23 09:00:08 -0500

    refactor: added cors headers

--- public/server.py
+++ public/server.py
@@ -3,18 +3,18 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 PORT = 8888
 ServerAddress = ("localhost", PORT)
 
-# class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
-#     def end_headers(self):
-#         self.send_header("Access-Control-Allow-Origin", "*")
-#         self.send_header("Access-Control-Allow-Methods", "GET, OPTIONS")
-#         self.send_header("Access-Control-Allow-Headers", "*")
-#         super().end_headers()
-#
-#     def do_OPTIONS(self):
-#         self.send_response(200, "OK")
-#         self.end_headers()
+class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "*")
+        super().end_headers()
 
-with HTTPServer(ServerAddress, SimpleHTTPRequestHandler) as httpd:
+    def do_OPTIONS(self):
+        self.send_response(200, "OK")
+        self.end_headers()
+
+with HTTPServer(ServerAddress, CORSHTTPRequestHandler) as httpd:
     print("serving on port:", PORT)
     try:
         httpd.serve_forever()
