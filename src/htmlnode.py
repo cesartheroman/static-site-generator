e7a5e3b921126b51a8d7c44d819f36b1903684da
cesartheroman <cesardroman18@gmail.com>
2025-01-23 13:15:43 -0600

    [refactor] forgot to remove extra "/>"

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -82,7 +82,7 @@ class LeafNode(HTMLNode):
             return self.value
 
         if self.tag == "img":
-            return f"<{self.tag}{self.props_to_html()}>{self.value}/>"
+            return f"<{self.tag}{self.props_to_html()}>"
 
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
